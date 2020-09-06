14e5f0dec5212630c0a654bdd6e8a034d97c44ee
Edoardo Sanna <edoardo.sanna.es@gmail.com>
2020-09-06 16:02:38 +0200

    Update Note-MRTdocker.ps1

--- MRT-docker/Note-MRTdocker.ps1
+++ MRT-docker/Note-MRTdocker.ps1
@@ -18,6 +18,8 @@ Docker pull mcr.microsoft.com/dotnet/framework/aspnet:4.8
 # Pull Windows 1809 as oledlg.dll source for Crystal Reports 
 # https://stackoverflow.com/questions/55638049/docker-container-with-support-for-crystal-reports
 Docker pull mcr.microsoft.com/windows:1809
+# MS SQL Server Express image
+docker pull microsoft/mssql-server-windows-express:latest
 
 # Check dockerfile here
 # .......
@@ -31,31 +33,17 @@ docker run --name mpassw --publish 8080:80 --detach mpasswimage
 # Run powershell on the container
 docker exec -it mpassw powershell
 
-# Powershell in the container: web configuration
-> Remove-IISSite -Name "Default Web Site"
-> New-IISSite -Name "mpassw" -BindingInformation "*:80:" -PhysicalPath "$env:systemdrive\MPW\Micronpass" -PassThru
-> Get-IISSite -Name "mpassw"
-
 # Powershell in the container: install Crystal Reports
 $msiArguments64 = '/qn','/i','"CRRuntime_64bit_13_0_25.msi"'
 Start-Process -PassThru -Wait msiexec -ArgumentList $msiArguments64
 $msiArguments32 = '/qn','/i','"CRRuntime_32bit_13_0_25.msi"'
 Start-Process -PassThru -Wait msiexec -ArgumentList $msiArguments32
-# ----> Questa parte va in errore!
-
-# Run cmd on the container
-docker exec -it mpassw cmd
-
-# Cmd in the container: modify permissions on folders 
-> icacls C:\MPW\Micronpass /grant Everyone:(F)
-> icacls \inetpub\wwwroot /grant Everyone:(F)
 
 # Indirizzo IP del container
 $IP = docker inspect --format '{{ .NetworkSettings.Networks.nat.IPAddress }}' mpassw
 
 # Back to host: test web application with default browser
 Start-Process "http://$IP:8080"
-
 
 # ----------------
 
