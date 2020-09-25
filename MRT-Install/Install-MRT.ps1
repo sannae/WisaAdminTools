79a18d8fc90880d95c02d0ead2cd46a7fff5d328
Edoardo Sanna <edoardo.sanna.es@gmail.com>
2020-09-25 09:13:21 +0200

    With packages

--- MRT-Install/Install-MRT.ps1
+++ MRT-Install/Install-MRT.ps1
@@ -13,18 +13,13 @@ The script performs 1) installation of IIS on Windows client or server,
 
 .NOTES
 Requisites:
-    - CSV file with required IIS features in the same directory (maybe replace with JSON?)
-    - SQLEXPR_x64_ENU.exe in same directory (if needed) (English only for now)
-    - SSMS-SETUP-ENU.exe in same directory (if needed) (English only for now)
-    - MRTxxx.exe in same directory
-    - Modules/SqlServer PowerShell module (last release downloadable from https://www.powershellgallery.com/packages/Sqlserver)
-To-do:
-    - Insert CheckProgramInstallation function
-    - Add speed-test
-    - Add DSC test at the end of the script
+    - ./IIS_features.csv: CSV file with required IIS features in the same directory (maybe replace with JSON?)
+    - ./Packages/SQLEXPR_x64_ENU.exe in same directory (if needed) (English only for now)
+    - ./Packages/SSMS-SETUP-ENU.exe in same directory (if needed) (English only for now)
+    - ./Packages/MRTxxx.exe in same directory
+    - ./Modules/dbatools: PowerShell dbatools module
+    - ./Modules/IISAdministration: PowerShell IISAdministration module
 
-.EXAMPLE
-./Install-MRT.ps1 
 .EXAMPLE
 ./Install-MRT.ps1 --InstallSQL --InstallSSMS
 
