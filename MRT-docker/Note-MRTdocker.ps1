0f850464607eab8c51a01cbccb5ecdb805fbc933
Edoardo Sanna <edoardo.sanna.es@gmail.com>
2020-09-25 20:46:57 +0200

    Note

--- MRT-docker/Note-MRTdocker.ps1
+++ MRT-docker/Note-MRTdocker.ps1
@@ -6,6 +6,7 @@ Alcuni link utili:
     * FreeCodeCamp's Docker Handbook : https://www.freecodecamp.org/news/the-docker-handbook/
     * Stackify's start-to-finish guide to Docker for .NET : https://stackify.com/a-start-to-finish-guide-to-docker-for-net/
 * Octopus Blog : https://octopus.com/blog/containerize-a-real-world-web-app
+* Docker lab : https://github.com/docker/labs/tree/master/dockercon-us-2017/windows-modernize-aspnet-ops#task3.1
 #>
 
 
