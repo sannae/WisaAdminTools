e59c61ff3f8ceb5ac71049afd44baba1e4cbbc9b
sannae <edoardo.sanna.es@gmail.com>
2021-01-05 16:56:07 +0100

    Updated psm1 file with Requires

--- MrtAdminTools.psm1
+++ MrtAdminTools.psm1
@@ -2,33 +2,29 @@
 #-----------------------------------------------------------------------------------------------------
 # Write PowerShell to automate your business processes in order to reduce human input, errors and cost!
 
-# Controllo di versione di Powershell: ALMENO la 5.0!
-
-<#
-if ( $PVersionTable.PSVersion.Major -lt 5 ) {
-    Write-Host "La tua versione di Powershell è obsoleta! Non posso eseguire questo script..."
-    Write-Host "Per aggiornare Windows Powershell: https://docs.microsoft.com/en-us/powershell/scripting/windows-powershell/install/installing-windows-powershell?view=powershell-7"
-    Write-Host "Per passare direttamente a Powershell Core: https://github.com/PowerShell/PowerShell"
-    break
-}
-#>
+# Requirements
+#Requires -Version 5.1
+#Requires -Modules IISAdministration
+#Requires -RunAsAdministrator
 
 # Export public functions (tested ones)
-$PublicFunctionsFiles = [System.IO.Path]::Combine($PSScriptRoot,"Functions","Public","*.ps1")
+$PublicFunctionsFiles = [System.IO.Path]::Combine($PSScriptRoot, "Functions", "Public", "*.ps1")
 Get-ChildItem -Path $PublicFunctionsFiles -Exclude *.tests.ps1, *profile.ps1 | ForEach-Object {
     try {
         . $_.FullName
-    } catch {
+    }
+    catch {
         Write-Warning "$($_.Exception.Message)"
     }
 }
 
 # Export private functions (maintenance)
-$PrivateFunctionsFiles = [System.IO.Path]::Combine($PSScriptRoot,"Functions","Private","*.ps1")
+$PrivateFunctionsFiles = [System.IO.Path]::Combine($PSScriptRoot, "Functions", "Private", "*.ps1")
 Get-ChildItem -Path $PrivateFunctionsFiles -Exclude *.tests.ps1, *profile.ps1 | ForEach-Object {
     try {
         . $_.FullName
-    } catch {
+    }
+    catch {
         Write-Warning "$($_.Exception.Message)"
     }
 }
