048a5558f8a813f2fae8dbe8b421c96815247995
sannae <edoardo.sanna.es@gmail.com>
2021-01-19 17:32:11 +0100

    added get-applogs

--- Functions/Private/Get-AppLogs.ps1
+++ Functions/Private/Get-AppLogs.ps1
@@ -3,16 +3,15 @@
     Restituisce una lista di array contenenti DataOra, LogName e contenuto della riga del log per un'applicazione specificata.
 .DESCRIPTION
     Lo script legge riga per riga tutti i file di log all'interno della cartella di una specifica applicazione.
-    L'applicazione può essere un servizio di Windows o un'app web e viene indicata nella variabile $ApplicationName.
+    L'applicazione può essere un servizio di Windows o un'app web e viene indicata nella variabile $Path.
     Lo script cerca ricorsivamente tutti i file .log contenuti nella cartella dell'applicazione, ne effettua il parsing della data e del contenuto.
     Data e contenuto di ogni riga sono poi usati per costruire un PSCustomObject.
     La data è filtrata con i parametri $StartDate e $StopDate.
     Eventualmente è possibile fare in modo che vengano filtrate solo le righe contenenti una cerca stringa, indicata nella variabile $SearchString.
     L'oggetto ritornato è un ArrayList che viene outputato su host ordinato per DateTime crescente.
-.PARAMETER APPLICATIONNAME
-    È il nome dell'applicazione, win o web, di cui estrarre i log. È un parametro obbligatorio.
-    Il nome va specificato in maniera esatta, perché viene usato per cercare la cartella omonima da cui estrarre i log.
-    Ecco perché è possibile scegliere il valore di questo parametro da un ValidateSet.
+.PARAMETER PATH
+    Percorso della cartella contenente i file di log: vengono cercati tutti i file *.log contenuti all'interno.
+    Il parametro viene validato verificando l'esistenza del percorso.
 .PARAMETER STARTDATE
     È la data di inizio del periodo indicato.
     Va inserita in formato 'dd-MM-yyyy hh:mm:ss'.
@@ -25,34 +24,31 @@
     È la stringa che si vuole cercare nei log, da usare come filtro.
     Se non specificata, è uguale a "*", ovvero non viene fatto alcun filtro.
 .EXAMPLE
-    PS> Get-MrtServiceEventLog -ApplicationName Micronpass
-    Restituisce tutti i log dell'applicativo Micronpass della giornata odierna
+    PS> Get-MrtServiceEventLog -Path MYAPPFOLDER
+    Restituisce tutti i log dell'applicativo MYAPPFOLDER della giornata odierna
 .EXAMPLE
-    PS> Get-MrtServiceEventLog -ApplicationName MicronService -StartDate '01-01-2020 00:00:00' -StopDate '02-01-2020 00:00:00'
-    Restituisce tutti i log dell'applicativo MicronService nella giornata del 01-01-2020
+    PS> Get-MrtServiceEventLog -Path MYAPPFOLDER -StartDate '01-01-2020 00:00:00' -StopDate '02-01-2020 00:00:00'
+    Restituisce tutti i log dell'applicativo MYAPPFOLDER nella giornata del 01-01-2020
 .EXAMPLE
-    PS> Get-MrtServiceEventLog -ApplicationName NoService -SearchString "Disconnessione"
-    Restituisce tutti i log dell'applicativo NoService contenenti la stringa "Disconnessione" 
+    PS> Get-MrtServiceEventLog -PAth MYAPPFOLDER -SearchString "Disconnessione"
+    Restituisce tutti i log dell'applicativo MYAPPFOLDER contenenti la stringa "Disconnessione" 
 .NOTES
     0.9 (refactoring)
-    TODO: Cambiare il parametro $SearchString da stringa singola ad array di stringhe
-    TODO: Gestire il parametro $SearchString con -Include ed -Exclude, così da poter omettere le righe inutili
+    TODO : Error handling!!
+    TODO : Cambiare il parametro $SearchString da stringa singola ad array di stringhe
+    TODO : Gestire il parametro $SearchString con -Include ed -Exclude, così da poter omettere le righe inutili
 #>
 
-function Get-MrtEventLogs {
+function Get-AppLogs {
 
     [CmdletBinding()]
     param (
         [Parameter(
             Mandatory = $True,
-            HelpMessage = "Digitare il nome completo dell'applicazione",
+            HelpMessage = "Digitare il percorso completo della cartella",
             ValueFromPipeline = $true)]
-        [ValidateSet(
-            "MicronService", 
-            "NoService",
-            "Micronpass",
-            "MicronConfig")]
-        [string]$ApplicationName,
+        [ValidateScript( { Test-Path $_ } )]
+        [string]$Path,
         [Parameter(
             HelpMessage = "Digitare data ora di inizio (dd-MM-yyyy hh:mm:ss)")]
         [String]$StartDate = (Get-Date -Format 'dd-MM-yyyy 00:00:00').ToString(),
@@ -62,13 +58,14 @@ function Get-MrtEventLogs {
         [string]$SearchString = "*"
     )
 
-    # Cartella root MPW
-    $RootFolder = Get-AppSuiteRootFolder
-    $Path = "$RootFolder\$ApplicationName"
-
     # Salva i log in una variabile
     $Logs = Get-ChildItem -Path $Path -Recurse | Where-Object { $_.Name -like "*.log" }
-    Write-Verbose "Ho trovato i seguenti log: $Logs"
+    if ( $null -eq $Logs ) {
+        Write-Error "Non ci sono file log nella cartella indicata!"
+    }
+    else {
+        Write-Verbose "Ho trovato i seguenti log: $Logs"
+    }
 
     # Crea array per salvare i risultati
     $LogResults = [System.Collections.ArrayList]@()
@@ -80,7 +77,7 @@ function Get-MrtEventLogs {
         Write-Verbose "Mi sto leggendo tutto il log $LogName..."
 
         # Contenuto del log, splittato per riga
-        $logContent = Get-Content $Log | Select-Object -skip 1 # Rimuove la prima riga di servicelog.log
+        $logContent = Get-Content "$Path\$Log" | Select-Object -skip 1 # Rimuove la prima riga
         $logContent -split "`n" | Where-Object { $_.trim() } | Out-null
 
         # Ciclo su ogni riga
@@ -90,9 +87,7 @@ function Get-MrtEventLogs {
             $RowDate = ($_ -split '\s', 3)[0] + ' ' + ($_ -split '\s', 3)[1]
 
             if ( ($RowDate -eq '') -or ($RowDate -eq ' ') ) {
-
                 continue
-            
             }
             else {
 
