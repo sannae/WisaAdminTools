a14ea8533c2111bc492c8a13f57173babb830a39
sannae <edoardo.sanna.es@gmail.com>
2021-02-03 12:34:47 +0100

    Tested Update-WebApplicationMinor

--- MrtAdminTools/Functions/Draft/Update-WebApplicationMinor.ps1
+++ MrtAdminTools/Functions/Draft/Update-WebApplicationMinor.ps1
@@ -1,21 +1,18 @@
 <#
 .SYNOPSIS
-    Aggiorna un'applicazione web all'ultima versione (entro la stessa minor).
+
 .DESCRIPTION
-    Lo script richiede il .zip dell'applicazione alla versione minor/patch più recente.
-    Ad esempio, questo script può aggiornare una 1.0.0 a una 1.0.1, ma NON a una 1.1.0.
-    I file della versione 'vecchia' vengono salvati in una cartella Applicazione_OLDVERSION
-    Attenzione : vengono esclusi dalla sovrascrittura i file web.config e LIC
+
 .PARAMETER APPFULLNAME
-    Nome completo dell'applicazione.
-    Digitare correttamente, in quanto verrà usato per cercare la cartella omonima sotto MPW.
+
 .PARAMETER ZIPPATH
-    Percorso in cui si trova il file zip contenente i file aggiornati.
-    Il percorso inserito viene validato dalla funzione stessa.
+
+.PARAMETER EXCLUDEFILES
+
 .EXAMPLE
-    PS> Update-WebApplication MYWEBAPP C:\temp
+
 .EXAMPLE
-    PS> Update-WebApplication -AppFullName MYWEBAPP -ZipPath C:\temp
+
 .NOTES
     0.0 (finire di testare, aggiungere test su Pester)
     TODO: Sostituire lo Start-Process URL al fondo con una funzione Test-WebApplication che restituisca errori HTTP
@@ -47,9 +44,10 @@ function Update-WebApplicationMinor {
     # Trovo application pool dell'applicazione e lo arresto
     Stop-WebApplicationPool -AppFullName $AppFullName
 
-    # Creo la cartella Webapplication_OLDVER
+    # Ricavo la versione precedente
     $OldVersion = [version]$(Get-Item "$RootFolder\$AppFullName\bin\$AppName.dll").versioninfo.fileversion
     
+    # Creo la cartella WebApplication_OldVersion
     $OldFolder = "$AppFullName" + "_" + "$OldVersion"
     if ( !(Test-Path "$RootFolder\$OldFolder")) {
         New-Item -Path "$RootFolder\$OldFolder" -ItemType Directory | Out-Null
@@ -57,10 +55,10 @@ function Update-WebApplicationMinor {
     else {
         Write-Warning "La cartella $RootFolder\$OldFolder esiste già!"
     }
-    Copy-Item -Path "$RootFolder\$AppFullName\*" -Destination "$RootFolder\$OldFolder" -Force
+    Copy-Item -Path "$RootFolder\$AppFullName\*" -Destination "$RootFolder\$OldFolder" -Recurse -Force
     Write-Verbose "I file della vecchia applicazione sono stati copiati in '$RootFolder\$OldFolder'"
 
-    # Estraggo installer dallo zip e ricavo nuova versione
+    # Estraggo installer dallo zip
     $ZipFile = Get-Item "$ZipPath\*$AppFullName*.zip"
     $ZipFolder = $ZipFile.BaseName
     if ( !(Test-Path "$ZipPath\$ZipFolder")) {
@@ -85,28 +83,28 @@ function Update-WebApplicationMinor {
     Write-Verbose "Sto installando il contenuto di $MsiFile..."
     Start-Process -PassThru -Wait "msiexec.exe" -ArgumentList $MSIArguments -Verb RunAs | Out-null
 
-    # Test (da spostare su Pester)
+    # Test installazione MSI
     if ( !(Test-Path "C:\inetpub\wwwroot\webupgrade") ) {
-        Write-Error "Qualcosa è andato storto nell'installazione di $ZipFile!"
+        Write-Error "Qualcosa è andato storto nell'installazione di $MsiFile!"
         break
     }
     else {
-        Write-verbose "$ZipFile installato con successo sotto \inetpub\wwwroot !"
+        Write-verbose "$MsiFile installato con successo sotto \inetpub\wwwroot !"
     }
 
-    # Sovrascrivo ricorsivamente i file appena unzippati in MPW\Micronpass (escludi web.config e licenza)
+    # Sovrascrivo ricorsivamente i file appena unzippati in MPW\Micronpass (escludi web.config e licenza) 
+    # SilentlyContinue serve ad evitare l'errore sui font
+    $SourceDir = "C:\inetpub\wwwroot\webupgrade\"
+    $DestDir = "$RootFolder\$AppFullName"
     Write-Verbose "Sto copiando i file da \inetpub\wwwroot a $RootFolder\$appFullName, escludendo i file $ExcludeFiles..."
-    # \\TODO: Aggiungere nuovamente -Exclude e testarlo
-    Copy-Item -Path "C:\inetpub\wwwroot\webupgrade\*" -Destination "$RootFolder\$AppFullName" -Recurse -Force
+    Get-ChildItem $SourceDir -Recurse -Exclude $ExcludeFiles | Copy-Item -Destination {Join-Path $DestDir $_.FullName.Substring($SourceDir.length)} -Recurse -Force -ErrorAction SilentlyContinue
 
     # Cancella inetpub\wwwroot\webupgrade e altri cadaveri penzolanti
     Remove-Item -LiteralPath "$ZipPath\$ZipFolder" -Force -Recurse
     Remove-Item -LiteralPath "C:\inetpub\wwwroot\webupgrade" -Force -Recurse
 
     # Riavvio app pool
-    Write-Verbose "Avvio dell'application pool in corso..."
     Start-WebApplicationPool -AppFullName $AppFullName
-    Start-Sleep -Seconds 2
 
     # Se qualcosa è andato storto, rollback alla versione precedente
     # $TestCondition = $(Invoke-WebRequest -Uri "http://localhost/$AppName" -UseBasicParsing).StatusCode -ne 200
@@ -114,34 +112,27 @@ function Update-WebApplicationMinor {
     $ZipVersion = $(Get-InstallFileInfo -Path $ZipFile).FileVersion
     $Testcondition = ($DllVersion.Major -eq $ZipVersion.Major) -and ($DllVersion.Minor -eq $ZipVersion.Minor) -and ($DllVersion.Build -eq $ZipVersion.Build)
 
-    ###
-    break
-
     # DA FINIRE
-    if ( !$TestCondition ) {
+    if ( !$TestCondition  ) {
 
         # Rollback !
         Write-Error "Aggiornamento non riuscito. Rollback a versione precedente in corso..."
 
         # Stoppa app pool
         Stop-WebApplicationPool -AppFullName $AppFullName
+ 
+        # Cancella il contenuto della cartella AppFullName
+        Remove-item -LiteralPath "$RootFolder\$AppFullName" -Recurse -Force -ErrorAction SilentlyContinue
 
-        # Cancella cartella Applicazione/
-        Remove-item -LiteralPath "$RootFolder\$AppFullName" -Recurse -Force # Non riesco a cancellare i file TFF !
-
-        # Rinomina cartella Applicazione_OLD/ in Applicazione/
-        if ( Test-Path "$RootFolder\$AppFullName") {
-            Copy-Item -Path "$RootFolder\$OldFolder\*" -Destination "$RootFolder\$AppFullName" -Recurse -Force # Non riesco a sovrascrivere i file TFF !
-        }
-        else {
-            Rename-item -Path "$RootFolder\$OldFolder" -NewName "$RootFolder\$AppFullName" -Force
-        }
+        # Copia il contenuto della cartella OldFolder dentro ad AppFullName
+        Copy-Item -Path "$RootFolder\$OldFolder\*" -Destination "$RootFolder\$AppFullName" -Recurse -Force -ErrorAction SilentlyContinue
 
         # Avvia app pool
         Start-WebApplicationPool -AppFullName $AppFullName
+
     }
     else {
-        Write-Host "L'applicazione $AppFullName è stata aggiornata con successo da $OldVersion a $DllVersion" -ForegroundColor Green
+        Write-Host "Ho finito di aggiornare l'applicazione $AppFullName da $OldVersion a $DllVersion" -ForegroundColor Green
     }
 
 }
