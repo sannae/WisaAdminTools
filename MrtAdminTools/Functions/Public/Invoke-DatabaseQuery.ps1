18a65f040dc7a4e5586042781a42dc91577dae0a
sannae <edoardo.sanna.es@gmail.com>
2021-02-17 14:19:04 +0100

    Edited Invoke-DatabaseQuery to close the SqlConnection

--- MrtAdminTools/Functions/Public/Invoke-DatabaseQuery.ps1
+++ MrtAdminTools/Functions/Public/Invoke-DatabaseQuery.ps1
@@ -3,24 +3,28 @@
     Esegue una query SQL specifica su un database di cui va fornita la stringa di connessione.
 .DESCRIPTION
     Lo script utilizza l'oggetto SqlClient.SqlConnection per aprire una connessione su un'istanza SQL Server.
-    Questo formato è usato per esempio nei file config della MRT app suite, ad eccezione dei web.config.
+    Utilizzando il framework .NET, non dipende da alcun tool esterno e quindi è portabile su qualsiasi OS con .NET > 4.0.
+    La connessione è aperta usando la stringa di connessione come parametro di input in uno specifico formato.
+    Questo formato è usato per esempio nei file config della App Suite, ad eccezione dei web.config.
     Nel caso di SELECT, i dati vengono scritti in output come array.
     È possibile richiamare i valori di una particolare colonna dell'array usando $_.NOMECOLONNA.
     Nel caso di UPDATE/DELETE, i dati non vengono scritti in output.
 .PARAMETER CONNECTIONSTRING
     Stringa di connessione al database; deve essere nel formato "User ID =;Password=;Initial Catalog=;Data Source="
-    Se non fornita dall'utente sottoforma di parametro, viene data per scontata la stringa di connessione ricavata dalla funzione Get-MrtConnectionStrings (v. help relativo).
+    Se non fornita dall'utente sottoforma di parametro, viene data per scontata la stringa di connessione ricavata dalla funzione Get-AppConnectionStrings (v. help relativo).
 .PARAMETER QUERY
     Query da eseguire, in formato stringa (es. "SELECT * FROM TABLE").
     Eventualmente si può acquisire da un file esterno usando $Query = $(Get-Content FILE.sql)
 .EXAMPLE
-    PS> Invoke-MPWDatabaseQuery -ConnectionString $ConnectionString -Query "SELECT * FROM TABLE"
+    PS> Invoke-DatabaseQuery -ConnectionString $ConnectionString -Query "SELECT * FROM TABLE"
     Esegue la query indicata connettendosi con la stringa di connessione specificata.
 .EXAMPLE
-    PS> Invoke-MPWDatabaseQuery -Query $(Get-Content File.sql)
-    Esegue la query contenuta in File.sql connettendosi all'istanza ricavata dalla funzione Get-AppConnectionStrings
+    PS> $Table = Invoke-DatabaseQuery -Query $(Get-Content File.sql)
+    Esegue la query contenuta in File.sql connettendosi all'istanza ricavata dalla funzione Get-AppConnectionStrings.
+    I risultati vengono salvati nella variabile $Table.
 .NOTES
     1.0 (testato in locale)
+    https://www.sqlshack.com/connecting-powershell-to-sql-server/
     TODO: Gestire l'autenticazione di Windows!
     TODO: Gestire gli errori!
 #>
@@ -36,17 +40,23 @@ function Invoke-DatabaseQuery
         [Parameter(Mandatory=$True)][string]$Query
     )
 
-    # Crea connessione
+    # Crea ed apri connessione
     $SqlConnection = New-Object -TypeName Data.SqlClient.SqlConnection
     $SqlConnection.ConnectionString = $ConnectionString
+    $SqlConnection.Open()
 
-    # Carica query
+    # Crea comando
     $SqlCommand = $SqlConnection.CreateCommand()
     $SqlCommand.CommandText = $Query
 
-    # Esegui query
+    # Crea Data Adapter (“represents a set of data commands and a database connection that are used to fill the DataSet”)
     $adapter = New-Object -TypeName Data.SqlClient.SqlDataAdapter $SqlCommand
+
+    # Crea Data Set (“an in-memory cache of data”)
     $dataset = New-Object -TypeName System.Data.DataSet
     $adapter.Fill($dataset) | Out-Null
     $dataset.Tables[0]
+
+    # Chiudi connessione
+    $SqlConnection.Close()
 }
