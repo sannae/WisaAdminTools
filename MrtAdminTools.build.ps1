01bdfac372b24fbe8fd2dc136d86b2710c46ed8a
sannae <edoardo.sanna.es@gmail.com>
2021-03-22 13:00:17 +0100

    fixed build file

--- MrtAdminTools.build.ps1
+++ MrtAdminTools.build.ps1
@@ -38,7 +38,7 @@ Enter-Build {
     Invoke-PSDepend -Force
 
     # Setting build script variables
-    $script:moduleName = 'SampleModule'
+    $script:moduleName = 'MrtAdminTools'
     $script:moduleSourcePath = Join-Path -Path $BuildRoot -ChildPath $moduleName
     $script:moduleManifestPath = Join-Path -Path $moduleSourcePath -ChildPath "$moduleName.psd1"
     $script:nuspecPath = Join-Path -Path $moduleSourcePath -ChildPath "$moduleName.nuspec"
