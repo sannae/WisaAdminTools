775e313fceb1843abed047a079355813dffa5b4b
sannae <edoardo.sanna.es@gmail.com>
2021-05-10 16:25:29 +0200

    Find out the pwd

--- WisaAdminTools.build.ps1
+++ WisaAdminTools.build.ps1
@@ -43,7 +43,7 @@ Enter-Build {
     Invoke-PSDepend -Force
 
     # Setting build script variables
-    $script:moduleName = $(Split-Path $PSCommandPath -Leaf).Split('.')[0]
+    $script:moduleName = 'WisaAdminTools' #$(Split-Path $PSCommandPath -Leaf).Split('.')[0]
     $script:moduleSourcePath = Join-Path -Path $BuildRoot -ChildPath $moduleName
     $script:moduleManifestPath = Join-Path -Path $moduleSourcePath -ChildPath "$moduleName.psd1"
     $script:nuspecPath = Join-Path -Path $moduleSourcePath -ChildPath "$moduleName.nuspec"
